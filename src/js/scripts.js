15f13eafc06313b07c3c06a3b8e9e876ebbb28cc
artlenk.ru@gmail.com <df5ukoi7ht>
2017-02-11 14:27:52 +0300

    Created audience block

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,11 @@
 $(document).ready(function () {
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1
+    });
+
+    $("[data-fancybox]").fancybox({
+        // Options will go here
+    });
 });
